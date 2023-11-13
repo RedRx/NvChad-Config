ffdce833e8c8c1371ab32b0153f77453783fca7e
BearyDev <teerapat.tech@gmail.com>
2023-11-13 16:31:46 +0700

    update prettyhover;

--- plugins/pretty-hover.lua
+++ plugins/pretty-hover.lua
@@ -1,12 +1,5 @@
----@type NvPluginSpec
-local spec = {
-  {
-    {
-      "Fildo7525/pretty_hover",
-      keys = { "<leader>k" },
-      config = true,
-    },
-  },
+return {
+  "Fildo7525/pretty_hover",
+  event = "LspAttach",
+  opts = {},
 }
-
-return spec
